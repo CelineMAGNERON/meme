cbcafc81ed5f1dbc97fde45b31f6839937102460
Cel Magneron <magneron.cel@gmail.com>
2021-01-07 09:43:26 +0100

    :pushpin: Fix Jest version

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('Render App Component', () => {
+  it('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/gotcha/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
