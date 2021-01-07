302a34d2f1b0ba39377166e6544d9566ecc0595b
Cel Magneron <magneron.cel@gmail.com>
2021-01-07 19:53:16 +0100

    :lipstick: Stylization

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import React, { useState, useEffect } from "react";
 // ...
 
 function App() {
-  const [templates, setTemplates] = useState([]);
 
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
+
+  // connection to API imgflip
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes").then((x) =>
       x.json().then((response) => setTemplates(response.data.memes))
@@ -13,13 +16,40 @@ function App() {
   }, []);
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Le Meme Generator de l'Espace</h1> 
-      {templates.map((template) => {
-        return <img style={{ width:200 }} key={template.id} src={template.url} alt={template.name}/>;
-      })}
+    <div className="container">
+       <h1 className="mt-4" style={{ textAlign: "center" }}>Le Meme Generator de l'Espace</h1>
+       <hr></hr>
+
+      <div className="columns">
+       
+        <div className="mt-4 column is-10">
+        <div className="columns is-flex-wrap-wrap"> 
+        {!template &&
+            templates.map((template) => {
+              return (
+                <div className="column is-3 ">
+                  <img
+                    style={{ width: 200 }}
+                    key={template.id}
+                    src={template.url}
+                    alt={template.name}
+                    onClick={() => {
+                      // setId(template.id)
+                    }}
+                  />
+                </div>
+              );
+            })}
+
+        </div>
+        
+        </div>
+        <div className="column is-flex">Coucou</div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
