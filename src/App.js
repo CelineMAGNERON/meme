1678990c3d0647c556f91627c6f519c432b39ef1
Cel Magneron <magneron.cel@gmail.com>
2021-01-07 15:09:22 +0100

    :alien: API imgflip connected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+// Two componants :
+// ...
 
 function App() {
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes").then((x) =>
+      x.json().then((response) => setTemplates(response.data.memes))
+    );
+  }, []);
+
   return (
-    <div>
-        <h1>Gotcha world !</h1>
-        <p>Keep go on girl </p>
+    <div style={{ textAlign: "center" }}>
+      <h1>Le Meme Generator de l'Espace</h1> 
+      {templates.map((template) => {
+        return <img style={{ width:200 }} key={template.id} src={template.url} alt={template.name}/>;
+      })}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 describe('Render App Component', () => {
   it('renders learn react link', () => {
     render(<App />);
-    const linkElement = screen.getByText(/gotcha/i);
+    const linkElement = screen.getByText(/espace/i);
     expect(linkElement).toBeInTheDocument();
   });
 });
