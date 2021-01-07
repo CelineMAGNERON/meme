be1a2290d40bfd1825c657cdbbe9746f9b76bc82
Cel Magneron <magneron.cel@gmail.com>
2021-01-07 20:13:02 +0100

    :beers: Sucker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 // Two componants :
 // ...
+// https://developer.mozilla.org/fr/docs/Web/HTML/Element/Input/image
+// https://fr.reactjs.org/docs/conditional-rendering.html
 
 function App() {
 
@@ -17,7 +19,8 @@ function App() {
 
   return (
     <div className="container">
-       <h1 className="mt-4" style={{ textAlign: "center" }}>Le Meme Generator de l'Espace</h1>
+       <h1 className="mt-4" style={{ textAlign: "center" }}>Le Meme Generator de l'Espace qui te fait voyager dans l'Absolu parce-que l'Absolu c'est Rien et Tout à la fois et qu'on aime la philo de l'Essentielle Pauvreté intersidérale. 
+       Cela dit je ne dirais pas que ce titre qui s'invite lors de cette flanerie profondemment intellectuelle est vraiment opportun, après tout est-ce réellement nécessaire ou serait-ce par pure mondanité d'un esprit qui chauffe un peu trop fort ? Cela questionne les limites d'une éventuelle vanité : Mettre ou ne pas mettre est-ce la question ?</h1>
        <hr></hr>
 
       <div className="columns">
